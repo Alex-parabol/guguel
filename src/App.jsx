cf3e97081eadb9621de86daae7fe23d718d5fbcb
Alex-parabol <68709781+Alex-parabol@users.noreply.github.com>
2022-07-05 00:12:45 +0200

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import React, {useState} from 'react'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import Routes from './components/Routes'
+import React, { useState } from "react";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Routes from "./components/Routes";
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
 
-    const [darkTheme, setDarkTheme] = useState(false);
+  return (
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-100 min-h-screen">
+        <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
+        <Routes />
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div classname={darkTheme ? 'dark' : ''}>
-            <div className="bg-gray-500">
-            <Navbar/>
-            <Footer/>
-            <Routes/>
-            </div>
-        </div>
-    )
-}
-
-export default App
+export default App;
