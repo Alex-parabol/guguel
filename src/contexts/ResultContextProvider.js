41695760b819cd611bbe56442d8be6ef12b84fe2
Alex <Alexlabora92@gmail.com>
2022-07-07 15:30:37 +0200

    api implemented

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState } from 'react';
 
 const ResultContext = createContext();
-const BaseUrl = 'https://google-search1.p.rapidapi.com/google-search' //podemos quitar /google-search e instaurar la busqueda nuestra más adelante.
+const BaseUrl = 'https://google-search3.p.rapidapi.com/api/v1' //podemos quitar /google-search e instaurar la busqueda nuestra más adelante.
 
 export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState([]);
@@ -17,9 +17,10 @@ export const ResultContextProvider = ({ children }) => {
         const response = await fetch(`${BaseUrl}${type}`, {
             method: 'GET',
             headers: {
+                'X-User-Agent': 'desktop',
                 'X-RapidAPI-Key': '61ec8ff567msh8628137f983d9efp127d4bjsnfaf503f50cc3',
-                'X-RapidAPI-Host': 'google-search1.p.rapidapi.com'
-            }
+                'X-RapidAPI-Host': 'google-search3.p.rapidapi.com'
+              }
         });
         const data = await response.json()
         console.log(data)
