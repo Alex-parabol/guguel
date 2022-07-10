8ee44ca013242b19a8daefb3a47712f1bc9ed37e
Alex <Alexlabora92@gmail.com>
2022-07-10 20:33:08 +0200

    search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+import {useDebounce} from 'use-debounce';
+
+import {useResultContext } from '../contexts/ResultContextProvider'
 import {Links} from './Links'
 
 
 export const Search = () => {
+
+  const [text,setText]= useState('cats')
   return <div>
     Search
     <Links />
